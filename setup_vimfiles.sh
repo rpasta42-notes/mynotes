2b22b808f883118aa2b8b278b20ff51f3bb0469e
Konstantin Kowalski <konstantin@businesscape.com>
2016-12-26 00:24:39 +0000

    Added update_git_vimfiles.sh

--- setup_vimfiles.sh
+++ setup_vimfiles.sh
@@ -10,7 +10,7 @@ mkdir -p ~/.vim/view
 view_dir_path=$(eval echo "~/.vim/view/")
 
 notes_n=1
-view_name="=+orgs=+rpasta42-personal=+mynotes=+numbered=+$notes_n.txt="
+view_name="~=+orgs=+rpasta42-personal=+mynotes=+numbered=+$notes_n.txt="
 notes_view_path=${view_dir_path}/${view_name}
 rm $notes_view_path
 cp numbered/${notes_n}.txt.vim $notes_view_path
